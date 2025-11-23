f86f5fde5a86a93bc3cac4fcde43db3c107461ca
Sumin Jeon <wjstnals1211@gmail.com>
2025-11-23 22:59:04 +0900

    feat: 주요 도시 목록 정의

---
+++ city_selector.go
@@ -0,0 +1,17 @@
+package main
+
+type cityOption struct {
+	nameKR string
+	nameEN string
+}
+
+var cityOptions = []cityOption{
+	{nameKR: "서울", nameEN: "Seoul"},
+	{nameKR: "부산", nameEN: "Busan"},
+	{nameKR: "인천", nameEN: "Incheon"},
+	{nameKR: "대구", nameEN: "Daegu"},
+	{nameKR: "대전", nameEN: "Daejeon"},
+	{nameKR: "광주", nameEN: "Gwangju"},
+	{nameKR: "울산", nameEN: "Ulsan"},
+	{nameKR: "제주", nameEN: "Jeju"},
+}
